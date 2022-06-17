e5060bbce6b9355bdd4681893bd528363ec86b40
Hugo Ferreira <hugodinis@ua.pt>
2022-06-17 23:46:28 +0100

    final push

--- projeto_final/projeto_final.ino
+++ projeto_final/projeto_final.ino
@@ -5,8 +5,6 @@ long thr = 0;
 long thm = 0;
 long thl = 0;
 const int vibrateTime = 1000;
-const int buzzerPin = 3;
-bool playSong = false;
 
 void setup() {
   Serial.begin(19200);
@@ -24,9 +22,6 @@ void loop() {
       //Serial.println("hr");
       digitalWrite(active_chan1, HIGH); 
       thr = millis() + vibrateTime;
-      //Serial.println(millis());
-      //Serial.println(thr);
-      //playSong = true;
     }  
     else if(s == "hm"){
       //Serial.println("hm");
@@ -38,16 +33,11 @@ void loop() {
       digitalWrite(active_chan3, HIGH); 
       thl = millis() + vibrateTime;
     }  
-    else{
-      //Serial.println("no");
-    }
   }
 
   if (thr < millis() and thr > 0){
     thr = -1;
     digitalWrite(active_chan1, LOW);  
-    playSong = false;
-    //analogWrite(buzzerPin, 0);
   }
   if (thm < millis() and thm > 0){
     thm = -1;
@@ -57,9 +47,4 @@ void loop() {
     thl = -1;
     digitalWrite(active_chan3, LOW);  
   }
-
-  if (playSong){
-    analogWrite(buzzerPin, 400);
-  }
-
 }
